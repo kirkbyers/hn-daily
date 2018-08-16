c38ae78aa48bb023c3d360e691a9d980f86b9316
kirkbyers <kirklbyers@gmail.com>
2018-08-15 22:26:24 -0500

    Create method for processing day of posts

---
+++ process/main.go
@@ -0,0 +1,46 @@
+package process
+
+import (
+	"sort"
+	"time"
+
+	"github.com/kirkbyers/hn-daily/db"
+)
+
+// Day returns the top 30 post 24 hr from given time
+func Day(t *time.Time) (result []db.HnPost, err error) {
+	var posts []db.HnPost
+	posts, err = db.GetScrapesForDay(t)
+
+	// Build a map of post with highest seen scores of the day
+	var postMap map[string]db.HnPost
+	for _, v := range posts {
+		if post, ok := postMap[v.ID]; ok {
+			old := &post
+			new := &v
+			if new.Score > old.Score {
+				postMap[v.ID] = *new
+			}
+			continue
+		}
+		postMap[v.ID] = v
+	}
+
+	// Build sortedPosts array
+	var sortedPosts []db.HnPost
+	for _, post := range posts {
+		sortedPosts = append(sortedPosts, post)
+	}
+	// Sort Sorted Posts
+	sort.Slice(sortedPosts, func(i, j int) bool {
+		return sortedPosts[i].Score < sortedPosts[j].Score
+	})
+
+	// Cut sorted posts to length of normal Frontpage
+	if len(sortedPosts) >= 30 {
+		result = sortedPosts[:30]
+	} else {
+		result = sortedPosts
+	}
+	return result, nil
+}
